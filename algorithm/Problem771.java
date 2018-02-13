c5c027b50bd932735d46de8f4c3dd36466f1c15e
Jinchul <jinchul@gmail.com>
2018-02-13 14:43:32 +0000

    Add solution

---
+++ algorithm/Problem771.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        Set<Character> set = new HashSet<Character>();
+        int count = 0;
+        for (int i = 0; i < J.length(); ++i) {
+            set.add(J.charAt(i));
+        }
+        for (int i = 0; i < S.length(); ++i) {
+            if (set.contains(S.charAt(i))) ++count;  
+        }
+        return count;
+    }
+}
